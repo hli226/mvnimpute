396783e2b578217fe94dbc7f5e7dc80ac8f2ff0b
hli226 <lihesen2004@gmail.com>
2022-05-03 13:48:24 -0500

    5-3-2022

--- R/multiple.imputation.R
+++ R/multiple.imputation.R
@@ -106,7 +106,6 @@ multiple.imputation <- function(
   ## Gibbs iteration
   for (i in 1:iter) {
 
-
     ##############################################
     ## 1. Use updated parameters to update data ##
     ##############################################
